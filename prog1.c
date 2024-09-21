f4bd06bae1965a590f1c453478a7dc0fc1b49401
shhrushti-patil <shrushpatil517@gmail.com>
2024-09-21 10:39:04 +0530

    stash

--- prog1.c
+++ prog1.c
@@ -4,4 +4,6 @@ void main()
 printf("this is line 1 in master");
 printf("this is line 2 in master");
 printf("this is line 1 in featurebranch");
+printf("this line before using stash");
+printf(" line 5");
 }
