9dbcc900984f4ea556dea2ffb132b5a7d1eb039b
shhrushti-patil <adityaname.2005@gmail.com>
2024-10-05 09:26:24 +0530

    merge

--- prog1.c
+++ prog1.c
@@ -5,6 +5,11 @@ printf("this is line 1 in master");
 printf("this is line 2 in master");
 printf(" hi");
 printf("this is line 1 in featurebranch");
+<<<<<<< Updated upstream
 printf("this line before using stash");
 printf(" line 5");
+=======
+xyz;
+printf("this line im writting before stashing");
+>>>>>>> Stashed changes
 }
